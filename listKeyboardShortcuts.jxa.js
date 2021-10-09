80e2a0a9b822a594160d3f2863b281483c7f1d12
drjlevi6 <drjlevi6@aol.com>
2021-10-09 18:02:43 -0400

    Update listKeyboardShortcuts.jxa.js
    Moved code to print command and shortcut into function printCellCommandAndShortcut()

--- listKeyboardShortcuts.jxa.js
+++ listKeyboardShortcuts.jxa.js
@@ -18,35 +18,39 @@ var kbWindow = sysPrefs.windows.byName("Keyboard");
 let kbwOutline = kbWindow.uiElements[1].uiElements[9].uiElements[2].uiElements[0];
 		
 /* each table row contains 2 cells; second cell contains either a disclosure 
-   triangle 
- */
+   triangle or a command with its shortcut. */
+
 for(let i = 0; i<kbwOutline.rows.length; i++){
 	let cell2 = kbwOutline.rows[i].uiElements.whose( {description: "cell"} )[1];
 	let numTextFields = 
 		cell2.uiElements.whose( {description: 'text field'} ).length;
 	if (numTextFields > 0) {
-
-		let cellNumStr = 
-			'cell[' + String(i) + ']:';
-		let cellNumStrPadded = 
-			cellNumStr + ' '.repeat(10-cellNumStr.length);
-		let commandStr =
-			cell2.uiElements.whose(
-				 {description: 'text field'} 
-			)[0].value();
-		let commandStrPadded = 
-			commandStr + ' '.repeat(40-commandStr.length);
-		let shortcut = cell2.uiElements.whose(
-			{description: 'text field'} 
-		)[numTextFields-1].value();
-
-		console.log(cellNumStrPadded +  commandStrPadded + shortcut);
-			
+			printCellCommandAndShortcut(i, cell2, numTextFields);
 	}
+
 }
 exit0(); /* we don't need this, but it may be useful if we wish to
 		end the program early (while debugging, say) */
 /*--Functions-----------------------------------------------------------------*/
+
+// Print formatted row containing
+function printCellCommandAndShortcut(i, cell2, numTextFields) {
+	let cellNumStr = 
+	'Row ' + String(i) + ':';
+	let cellNumStrPadded = 
+	cellNumStr + ' '.repeat(9-cellNumStr.length);
+	let commandStr =
+	cell2.uiElements.whose(
+		{description: 'text field'} 
+	)[0].value();
+	let commandStrPadded = 
+	commandStr + ' '.repeat(35-commandStr.length);
+	let shortcut = cell2.uiElements.whose(
+	{description: 'text field'} 
+	)[numTextFields-1].value();
+
+	console.log(cellNumStrPadded +  commandStrPadded + shortcut);
+}
 function exit0() {
 	return(
 		curApp.doShellScript('echo 0; exit 0')
