8e8c936d5c494ea9c20ba06ecc1e4d474571493c
drjlevi6 <drjlevi6@aol.com>
2021-10-09 12:52:02 -0400

    Update listKeyboardShortcuts.jxa.js
    Added comments indicating sections for main program, functions; added shell script exit0()

--- listKeyboardShortcuts.jxa.js
+++ listKeyboardShortcuts.jxa.js
@@ -10,26 +10,31 @@
 var curApp = Application.currentApplication();
 curApp.includeStandardAdditions = true;
 
-var sysEvs = Application('System Events');
+/*--Main----------------------------------------------------------------------*/
+var sysEvents = Application('System Events');
+	console.log("\nsysEvents:", sysEvents, '\n');
+	exit0();
 var sysPrefsApp = Application('System Preferences');
-var sysPrefsP = sysEvs.applicationProcesses.byName("System Preferences");
-
-var kb1 = sysPrefsP.windows.byName("Keyboard");
+var sysnPrefsP = sysEvents.applicationProcesses.byName("System Preferences");
+var kbWindow = sysPrefsP.windows.byName("Keyboard");
+	console.log(kbWindow);
 var tblOutline = 
-	kb1.tabGroups[0].splitterGroups[0].scrollAreas[1].outlines[0];
+	kbWindow.tabGroups[0].splitterGroups[0].scrollAreas[1].outlines[0];
 	console.log(tblOutline.role());
-
 //	tblOutline.rows[2].uiElements[1].uiElements.class();	
 		// ["staticText", "staticText", "uiElement"]
 //	tblOutline.rows[2].uiElements[1].staticTexts[0].value();	
 		//"LibreOffice.app"
 //	tblOutline.rows[2].uiElements[1].staticTexts[1].value();	
 		//""
-
-//var libreOfficeDisclosureTriangle = 
-	tblOutline.rows[2].uiElements[1].uiElements[2];
+/*	var libreOfficeDisclosureTriangle = 
+	tblOutline.rows[2].uiElements[1].uiElements[2];*/
 //	libreOfficeDisclosureTriangle.value();						
 		// 0: closed; 1: open
 		
 var codeSymbols = tblOutline.rows[3].uiElements[1].uiElements[1].value();
 		//(Unicode string)
+/*--Functions-----------------------------------------------------------------*/
+function exit0() {
+	curApp.doShellScript('exit 0');
+}
