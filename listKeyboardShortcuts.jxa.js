85f955602630d503a54bd4cf814e4bbdf5902453
drjlevi6 <drjlevi6@aol.com>
2021-10-09 17:08:20 -0400

    Update listKeyboardShortcuts.jxa.js
    Rewrote the code to filter out the table rows with disclosure triangles (= no command shortcut) and to get the shortcut in the remaining rows

--- listKeyboardShortcuts.jxa.js
+++ listKeyboardShortcuts.jxa.js
@@ -12,11 +12,32 @@ var curApp = Application.currentApplication();
 /*--Main----------------------------------------------------------------------*/
 var sysEvents = Application('System Events');
 curApp.includeStandardAdditions = true;
-curApp.doShellScript('echo 0; exit 0')
 var sysPrefs = sysEvents.applicationProcesses.byName("System Preferences");
 
 var kbWindow = sysPrefs.windows.byName("Keyboard");
-kbWindow;
+let kbwOutline = kbWindow.uiElements[1].uiElements[9].uiElements[2].uiElements[0];
+		
+/* each table row contains 2 cells; second cell contains either a disclosure 
+   triangle 
+ */
+for(let i = 0; i<kbwOutline.rows.length; i++){
+	let cell2 = kbwOutline.rows[i].uiElements.whose( {description: "cell"} )[1];
+	let numTextFields = 
+		cell2.uiElements.whose( {description: 'text field'} ).length;
+	if (numTextFields > 0) {
+		console.log('cell[' + i + ']: Command:',
+			cell2.uiElements.whose(
+				{description: 'text field'} 
+			)[0].value() + '; short cut:',
+			cell2.uiElements.whose(
+				{description: 'text field'} 
+			)[numTextFields-1].value());
+	}
+}
+//for (let i = 0; i < kbwOutline.rows[0].cells[i].length; i++) {
+	//kbwOutline.rows[i].uiElements.description();
+//	console.log("i:", i, kbwOutline.rows[0].cells[i].uiElements.description() );
+//}
 exit0();
 
 /*--Functions-----------------------------------------------------------------*/
