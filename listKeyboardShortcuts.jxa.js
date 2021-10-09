72977947a79aaff3f5a8ef47e025b4c7ce8b0e18
drjlevi6 <drjlevi6@aol.com>
2021-10-09 13:45:47 -0400

    Update listKeyboardShortcuts.jxa.js
    Removed mult. lines; still trying to get script to run at all

--- listKeyboardShortcuts.jxa.js
+++ listKeyboardShortcuts.jxa.js
@@ -8,33 +8,20 @@
 
 /*--Setup---------------------------------------------------------------------*/
 var curApp = Application.currentApplication();
-curApp.includeStandardAdditions = true;
 
 /*--Main----------------------------------------------------------------------*/
 var sysEvents = Application('System Events');
-	console.log("\nsysEvents:", sysEvents, '\n');
-	exit0();
-var sysPrefsApp = Application('System Preferences');
-var sysnPrefsP = sysEvents.applicationProcesses.byName("System Preferences");
-var kbWindow = sysPrefsP.windows.byName("Keyboard");
-	console.log(kbWindow);
-var tblOutline = 
-	kbWindow.tabGroups[0].splitterGroups[0].scrollAreas[1].outlines[0];
-	console.log(tblOutline.role());
-//	tblOutline.rows[2].uiElements[1].uiElements.class();	
-		// ["staticText", "staticText", "uiElement"]
-//	tblOutline.rows[2].uiElements[1].staticTexts[0].value();	
-		//"LibreOffice.app"
-//	tblOutline.rows[2].uiElements[1].staticTexts[1].value();	
-		//""
-/*	var libreOfficeDisclosureTriangle = 
-	tblOutline.rows[2].uiElements[1].uiElements[2];*/
-//	libreOfficeDisclosureTriangle.value();						
-		// 0: closed; 1: open
-		
-var codeSymbols = tblOutline.rows[3].uiElements[1].uiElements[1].value();
-		//(Unicode string)
+curApp.includeStandardAdditions = true;
+curApp.doShellScript('echo 0; exit 0')
+var sysPrefs = sysEvents.applicationProcesses.byName("System Preferences");
+
+var kbWindow = sysPrefs.windows.byName("Keyboard");
+kbWindow;
+exit0();
+
 /*--Functions-----------------------------------------------------------------*/
 function exit0() {
-	curApp.doShellScript('exit 0');
+	return(
+		curApp.doShellScript('echo 0; exit 0')
+	);
 }
